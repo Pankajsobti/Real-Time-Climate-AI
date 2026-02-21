a22515710bf232df2b72dac8eb6aa268b30fbbd6
Pankaj Sobti <pankajsobti10@gmail.com>
2026-02-21 22:04:41 +0530

    Real-time weather pipeline with multi-city and auto update

--- backend/weather_api.py
+++ backend/weather_api.py
@@ -1,11 +1,42 @@
 import requests
+import json
+import time
+from dotenv import load_dotenv
+import os
 
-API_KEY = "fa0e88be24ce09678792a832e2773f46"
+# Load API key from .env
+load_dotenv()
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
-city = "Delhi"
+# Indian cities
+cities = ["Delhi", "Mumbai", "Chennai", "Kolkata", "Ahmedabad"]
 
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+# Real-time loop
+while True:
+    weather_data = []
 
-response = requests.get(url)
+    for city in cities:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
-print(response.json())
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            weather_data.append({
+                "city": city,
+                "temperature": data["main"]["temp"],
+                "humidity": data["main"]["humidity"],
+                "weather": data["weather"][0]["description"]
+            })
+        else:
+            print("Error for", city, response.status_code)
+
+    # Save data
+    with open("data/weather_data.json", "w") as f:
+        json.dump(weather_data, f, indent=4)
+
+    print("Updated weather data")
+
+    # update every 30 sec
+    time.sleep(30)
